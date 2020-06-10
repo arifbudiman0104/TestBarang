423b05c20e3d7a046840dda39ca470726cdf64f4
Arif Budiman Arrosyid <arif.budiman.ft18@mail.umy.ac.id>
2020-06-10 09:19:42 +0700

    add code at string.xml and rename all activity

--- app/src/main/java/com/example/testbarang/TambahData.java
+++ app/src/main/java/com/example/testbarang/TambahData.java
@@ -2,12 +2,11 @@ package com.example.testbarang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.view.inputmethod.InputMethod;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,58 +16,55 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
 public class TambahData extends AppCompatActivity {
     private DatabaseReference database;
-    private Button btnSubmit;
-    private EditText etKode, etNama;
+
+    private Button btSubmit;
+    private EditText etKode;
+    private EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_data);
 
-        etKode = findViewById(R.id.editNo);
-        etNama = findViewById(R.id.editNama);
-        btnSubmit = findViewById(R.id.btnOK);
+        etKode = (EditText) findViewById(R.id.editNo);
+        etNama = (EditText) findViewById(R.id.editNama);
+        btSubmit = (Button) findViewById(R.id.btnOK);
 
-        //mengambil referensi ke firebase database
         database = FirebaseDatabase.getInstance().getReference();
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 if (!(etKode.getText().toString().isEmpty()) &&
-                        !(etNama.getText().toString().isEmpty())){
+                        !(etNama.getText().toString().isEmpty()) )
                     submitBrg(new Barang(etKode.getText().toString(),
                             etNama.getText().toString()));
-                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(i);
-                }
                 else
-                    Toast.makeText(getApplicationContext(), "Data tidak boleh kosong", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Data tidak boleh Kosong",
+                            Toast.LENGTH_LONG).show();
 
                 InputMethodManager imm = (InputMethodManager)
                         getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(etKode.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(etKode.getWindowToken(),0);
             }
         });
     }
-
     public void submitBrg(Barang brg){
-        /*Berikut ini adalah kode yang digunakan untuk mengirimkan data
-         * ke firebase RealTime Database dan juga kita set onSuccessListener
-         * yang berisi kode yang akan dijalankan ketika data berhasil ditambahkan*/
-        database.child("Barang").push().setValue(brg).addOnSuccessListener(this, new OnSuccessListener<Void>(){
-            @Override
-            public void onSuccess(Void aVoid){
-                etKode.setText("");
-                etNama.setText("");
-                Toast.makeText(getApplicationContext(), "Data berhasil ditambahkan", Toast.LENGTH_LONG).show();
-            }
-        });
+        database.child("Barang").push().setValue(brg).addOnSuccessListener(this,
+                new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        etKode.setText("");
+                        etNama.setText("");
+                        Toast.makeText(getApplicationContext(),"Data berhasil ditambahkan" ,
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
     }
-
     public static Intent getActIntent(Activity activity){
-        //kode untuk pengambilan intent
         return new Intent(activity, TambahData.class);
     }
 }
